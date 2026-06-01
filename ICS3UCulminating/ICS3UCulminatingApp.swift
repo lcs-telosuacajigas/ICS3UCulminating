619c43b694702a5621a01d3f067a07bf939af8dc
Tomas Elosua <telosuacajigas@lcs.on.ca>
2026-06-01 11:44:05 -0400

    Initial Commit

---
+++ ICS3UCulminating/ICS3UCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminatingApp.swift
+//  ICS3UCulminating
+//
+//  Created by Tomás Elosua Cajigas on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
